0a6e72566b12984f2ee4b558d7a20f8f8762a8c6
Laurent Peuch <cortex@worlddomination.be>
2017-05-14 20:27:59 +0200

    [mod] move forward

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.shortcuts import redirect
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^(.*)$', views.get_markdown, name=''),
+    url(r'^images/(.*)$', views.redirect_images_to_media, name=''),
+    url(r'^i18n.json$', lambda request: redirect("/static/i18n.json"), name=''),
+    url(r'^config.json$', lambda request: redirect("/static/config.json"), name=''),
+    url(r'^_pages/(.*)$', views.get_raw_markdown, name=''),
+    url(r'^(.*)$', views.get_page, name=''),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,61 @@
-from django.http import HttpResponse
+import os
+
+# from markdown2 import markdown_path
+from markdown import markdownFromFile
+
+from django.http import Http404, HttpResponse
+from django.conf import settings
+from django.shortcuts import render, redirect
 
 
-def get_markdown(request, file_name):
-    return HttpResponse("ok")
+def get_raw_markdown(request, file_name):
+    autorized_files_list = []
+    for i in os.listdir(os.path.join(settings.BASE_DIR, "git_content")):
+        if os.path.isdir(i):
+            continue
+
+        if not i.endswith(".md"):
+            continue
+
+        autorized_files_list.append(i)
+
+    if file_name in ("", "/"):
+        file_name = "index"
+
+    if file_name not in autorized_files_list:
+        raise Http404
+
+    return HttpResponse(open(os.path.join(settings.BASE_DIR, "git_content", file_name), "r"))
+
+
+def get_page(request, file_name):
+    autorized_files_list = []
+    for i in os.listdir(os.path.join(settings.BASE_DIR, "git_content")):
+        if os.path.isdir(i):
+            continue
+
+        if not i.endswith(".md"):
+            continue
+
+        autorized_files_list.append(i.rstrip(".md"))
+
+    if file_name in ("", "/"):
+        file_name = "index"
+
+    if file_name not in autorized_files_list:
+        raise Http404
+
+    # html = markdown_path(os.path.join(settings.BASE_DIR, "git_content", file_name + ".md"))
+    from StringIO import StringIO
+    html = StringIO()
+    markdownFromFile(input=os.path.join(settings.BASE_DIR, "git_content", file_name + ".md"), output=html)
+
+    html.read()
+
+    return render(request, "index.html", {
+        "content": html.buf,
+    })
+
+
+def redirect_images_to_media(request, image):
+    return redirect(settings.MEDIA_URL + "images/" + image)
