1bf1ff5913fc0525cad774a3aadf046d54fcd56c
Alexandre Aubin <alexandre.aubin@cern.ch>
2018-04-26 03:03:30 +0200

    Remove test thingy

--- frontend/urls.py
+++ frontend/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^i18n.json$', lambda request: redirect("/static/i18n.json"), name=''),
     url(r'^config.json$', lambda request: redirect("/static/config.json"), name=''),
     url(r'^_pages/(.*)$', views.get_raw_markdown, name=''),
-    url(r'^submit_page_change$', views.submit_page_change, name='test'),
+    url(r'^submit_page_change$', views.submit_page_change),
     url(r'^confirm/', include('generic_confirmation.urls',
         namespace='generic_confirmation'),
         name="confirm"),
